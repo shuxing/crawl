3bc07bb3bf2b303a2cf3d90998ed72bcfb4c3ed8
Shuxing <xiaoshuxing@gmail.com>
2017-02-20 06:36:48 -0800

    add config file

---
+++ src/config.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const config = {};
+module.exports = config;
+
+const db = {};
+config.db = db;
+db.type = 'docdb';
+
+db.docdb = {
+    name: 'crawl',
+    key: 'Do4BgQLNvdT8ooSUrogHGoLpbusQdffvq1cgcyo8cDBQLnSZgz9dQHbZJQ3xFtonN2no2UgbNyb6QEFUxKKiRw=='
+};
